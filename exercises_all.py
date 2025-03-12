39e8c574e7dc775adf2273708f11897ae02b5b62
Mark Peters <mapeters101@earthlink.net>
2025-03-11 23:36:39 -0400

    Reformat descriptions for all exercises.

--- exercises_all.py
+++ exercises_all.py
@@ -1,6 +1,7 @@
 '''
 Question 1
-Write some code that generates a file containing containing rows containing the following data:
+Write some code that generates a file containing containing rows containing
+the following data:
 
 i, fibonacci_i, factorial_i, gcd_fibonacci_i_factorial_i
 where:
@@ -8,7 +9,8 @@ where:
 i: integer values from 0 to 100
 fibonacci_i: the ith Fibonacci number
 factorial_i: the factorial of i (i!)
-gcd_fib_i_fact_i: the greatest common denominator of the ith Fibonacci number and i!
+gcd_fib_i_fact_i: the greatest common denominator of the ith Fibonacci number
+and i!
 Hint: look at the math.factorial and math.gcd functions in the Python docs
 
 Also make sure to include a header row in your file.
@@ -28,5 +30,9 @@ Using the file you just generated, write three functions:
 fib
 fact
 gcd_fib_fact
-that perform the same calculations as our original fib function, the math module's factorial and the gcd of the corresponding fibonacci and factorial numbers, but uses the data that was saved in the file as a cache/lookup mechanism - i.e. just use the numbers in the file if they are available, otherwise make the calculation.
+that perform the same calculations as our original fib function, the math
+module's factorial and the gcd of the corresponding fibonacci and factorial
+numbers, but uses the data that was saved in the file as a cache/lookup
+mechanism - i.e. just use the numbers in the file if they are available,
+otherwise make the calculation.
 '''
